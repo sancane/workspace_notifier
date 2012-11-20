f4c612eb2eb0a7d91108cd6f325d5c5ed1ca388a
Santiago Carot Nemesio <sancane@gmail.com>
2012-11-20 12:30:28 +0100

    Add event tests to the main class

---
+++ server.rb
@@ -0,0 +1,19 @@
+require 'goliath'
+
+class EventGenerator < Goliath::API
+  def response(env)
+    EM.add_periodic_timer(1) do
+      env.stream_send("data:hello ##{rand(100)}\n\n")
+    end
+
+    EM.add_periodic_timer(3) do
+      env.stream_send(["event:signup", "data:signup event ##{rand(100)}\n\n"].join("\n"))
+    end
+
+    streaming_response(200, {'Content-Type' => 'text/event-stream'})
+  end
+end
+
+class SSE < Goliath::API
+  # TODO: Match routes and send events to clients
+end
