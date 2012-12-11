b687ecbf80544e420fb59e04948bbcf760cf79d4
Santiago Carot Nemesio <sancane@gmail.com>
2012-12-11 16:37:02 +0100

    Add AMQP support

--- config/workspace_notifier.rb
+++ config/workspace_notifier.rb
@@ -1,11 +1,31 @@
 require 'mysql2'
+require 'amqp'
 require 'lib/notify_handler'
 
 config['notifier'] = NotifyHandler.new
 
+# Database configuration
 ActiveRecord::Base.establish_connection(:adapter  => 'em_mysql2',
                                         :database => 'your database',
                                         :username => 'user',
                                         :password => 'password',
                                         :host     => 'host',
                                         :pool     => 5)
+
+# AMQP configuration
+amqp_config = {
+  :host => 'localhost',
+  :user => 'test',
+  :pass => 'test',
+  :vhost => '/test'
+}
+
+connection = AMQP.connect(amqp_config)
+channel = AMQP::Channel.new(connection)
+queue = channel.queue("netlab.events.workspace", :auto_delete => true)
+exchange = channel.direct("")
+
+queue.subscribe do |msg|
+  # Parse event and send notification
+  puts "Received a message: #{msg}."
+end
