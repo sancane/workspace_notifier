213189adb362e4174e43c10900853f79f57ebb58
Santiago Carot Nemesio <sancane@gmail.com>
2012-12-19 12:55:24 +0100

    Set queue name depending on the execution environment

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -40,7 +40,7 @@ class Notifier
     chann = AMQP::Channel.new
     reply_queue = chann.queue("", :exclusive => true, :auto_delete => true) do |queue|
       chann.default_exchange.publish(@id.to_s,
-        :routing_key => "netlab.services.workspace.state",
+        :routing_key => "netlab.services.#{Goliath::env}.workspace.state",
         :message_id => Kernel.rand(10101010).to_s,
         :reply_to => queue.name)
     end
